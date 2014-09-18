2cd25239588635bfd098708d355edbde95a1e558
Hyunje Alex Jun <me@noraesae.net>
2014-09-18 01:20:24 +0100

    Fix wrong indents.

--- scripts/imgur-injected.js
+++ scripts/imgur-injected.js
@@ -1,8 +1,8 @@
 (function (document) {
   var handler = function (e) {
     var userInfo = {
-        name: e.target.nodeName,
-        url: e.target.src
+      name: e.target.nodeName,
+      url: e.target.src
     };
     safari.self.tab.setContextMenuEventUserInfo(event, userInfo)
   };
